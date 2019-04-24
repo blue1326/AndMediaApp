0bd29d913ab1be39ca516905f01f9ed75eb20d11
blue1326 <ragna1326@gmail.com>
2019-04-24 21:34:52 +0900

    Initial commit

---
+++ Assets/Script/SceneMgr.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneMgr : KeepScript
+{
+    public static GameObject SceneMgrInst = null;
+    public override void Awake()
+    {
+        base.Awake();
+        SceneMgrInst = this.gameObject;
+    }
+
+    private void Start()
+    {
+        
+    }
+
+
+
+    public void LoadScene(int _SceneNo)
+    {
+        SceneManager.LoadScene(_SceneNo);
+    }
+
+
+
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+}
---
+++ Assets/Script/SceneMgrGrabber.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SceneMgrGrabber : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        this.GetComponent<Button>().onClick.AddListener(SceneMgr.SceneMgrInst.GetComponent<SceneMgr>().BackToMainMenu);
+            //= SceneMgr.SceneMgrInst.GetComponent<SceneMgr>().BackToMainMenu();
+    }
+}
